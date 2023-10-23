0704bf0066f5f4f6f3ccc5dfd8b80263d2123c22
Einar Sommerfeldt <einarjso@stud.ntnu.no>
2023-10-23 17:34:54 +0200

    finished script

--- python_scripts/colmap_to_NSVF.py
+++ python_scripts/colmap_to_NSVF.py
@@ -2,6 +2,8 @@ from scipy.spatial.transform import Rotation
 import numpy as np
 import os
 import random
+import cv2 as cv
+from PIL import Image
 
 SCALING_FACTOR = 4 #Images will be downscaled by SCALING_FACTOR
 DATASET_SPLIT = np.array([100,10,10])
@@ -30,7 +32,7 @@ def create_img_info(imagestxt_path):
             
 #TODO:
 #pose X
-#rgb
+#rgb X
 #bbox
 #intrinsics
             
@@ -43,16 +45,56 @@ def populate_pose(output_folder, img_info, prefix):
         np.savetxt(output_folder+f"/pose/{prefix}{n:0=4}.txt", T, fmt="%1.9f")
     return 1
 
-def populate_rgb(output_folder, image_folder, img_info, prefix):
+def populate_rgb(output_folder, image_folder, calibration_folder, img_info, prefix):
+    K = np.loadtxt(calibration_folder +'/K.txt')/SCALING_FACTOR
+    K[2,2] = 1
+    DC = np.loadtxt(calibration_folder + '/dc.txt')
+
     for n,i in enumerate(img_info):
         [IMAGE_ID, QW, QX, QY, QZ, TX, TY, TZ, CAMERA_ID, NAME] = i
-        R = Rotation.from_quat([float(QW), float(QX), float(QY), float(QZ)]).as_matrix()
-        Rt = np.block([R,np.array([[float(TX)],[float(TY)],[float(TZ)]])])
-        T = np.block([[Rt],[np.array([0,0,0,1])]])
-        np.savetxt(output_folder+f"/pose/{prefix}{n:0=4}.txt", T, fmt="%1.9f")
+        #Copy images
+        img_path = image_folder + "/" + NAME
+        
+        orig_img = Image.open(img_path)
+        resized_img = orig_img.resize((orig_img.size[0]//SCALING_FACTOR, orig_img.size[1]//SCALING_FACTOR))
+        output_path = output_folder+f"/rgb/{prefix}{n:0=4}." + img_path.rsplit(".",1)[1].lower()
+        resized_img.save(output_path)
+
+        #Undistort image
+        I = cv.imread(output_path)
+        out = cv.undistort(I, K, DC)
+        cv.imwrite(output_path, out)
     return 1
 
-def colmap_to_NSVF(output_folder, image_folder, imagestxt_path):
+#Set bbox as space occupied by cameras
+def create_bbox(output_folder, img_info):
+    temp_list = list()
+    for i in img_info:
+        [IMAGE_ID, QW, QX, QY, QZ, TX, TY, TZ, CAMERA_ID, NAME] = i
+
+        #Create transform_matrix_field
+        center = np.array([float(TX), float(TY), float(TZ)])
+        temp_list.append(center)
+    
+    poses = np.array(temp_list)
+
+    #x_min, y_min, z_min = np.amin(poses,0)
+    #x_max, y_max, z_max = np.max(poses,0)
+
+    #x_min y_min z_min x_max y_max z_max initial_voxel_size
+    bbox = np.block([np.amin(poses,0), np.max(poses,0), 0.2])
+    np.savetxt(output_folder+f"/bbox.txt", bbox, fmt="%1.8f", newline=" ")
+    return 1
+
+def create_intrinsics(calibration_folder, output_folder): 
+
+    K = np.loadtxt(calibration_folder +'/K.txt')
+    K = K/SCALING_FACTOR
+    K[2,2] = 1
+
+    np.savetxt(output_folder+f"/intrinsics.txt", K, fmt="%1.6f")
+
+def colmap_to_NSVF(output_folder, image_folder, calibration_folder, imagestxt_path):
     random.seed(10)
     #create directories
     if not os.path.exists(output_folder):
@@ -69,14 +111,26 @@ def colmap_to_NSVF(output_folder, image_folder, imagestxt_path):
     train_size = int(DATASET_SPLIT[0]/np.sum(DATASET_SPLIT)*len(img_info))
     val_size = int(DATASET_SPLIT[1]/np.sum(DATASET_SPLIT)*len(img_info))
 
-    train_poses = img_info[0:train_size]
-    val_poses = img_info[train_size:train_size+val_size]
-    test_poses = img_info[train_size+val_size:]
+    train_set = img_info[0:train_size]
+    val_set = img_info[train_size:train_size+val_size]
+    test_set = img_info[train_size+val_size:]
     
-    populate_pose(output_folder,train_poses,"0_train_")
-    populate_pose(output_folder,val_poses,"1_val_")
-    populate_pose(output_folder,test_poses,"2_test_")
+    populate_pose(output_folder,train_set,"0_train_")
+    populate_pose(output_folder,val_set,"1_val_")
+    populate_pose(output_folder,test_set,"2_test_")
+
+    populate_rgb(output_folder, image_folder, calibration_folder, train_set, "0_train_")
+    populate_rgb(output_folder, image_folder, calibration_folder, val_set, "1_val_")
+    populate_rgb(output_folder, image_folder, calibration_folder, test_set, "2_test_")
+
+    create_bbox(output_folder, img_info)
+    create_intrinsics(calibration_folder, output_folder)
+
+    return 1
 
 
 imagestxt_path = r"C:\Users\einar\Desktop\fruit_colmap\images.txt"
-colmap_to_NSVF(r"dummy",r"C:\Users\einar\Desktop\fruit_colmap\images",imagestxt_path)
+output_folder = r"dummy"
+image_folder = r"C:\Users\einar\Desktop\fruit_colmap\images"
+calibration_folder = r"C:\Users\einar\Desktop\neodroid_plenoxels\camera_calibration\calibration"
+colmap_to_NSVF(output_folder, image_folder, calibration_folder, imagestxt_path)
