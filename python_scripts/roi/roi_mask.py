69d9cce0c717b4c7abc42ce0bc592d99513c5b52
Einar Sommerfeldt <einar.sommerfeldt@gmail.com>
2024-03-07 15:16:04 +0100

    added hull function

--- python_scripts/roi/roi_mask.py
+++ python_scripts/roi/roi_mask.py
@@ -18,9 +18,23 @@ def roi_mask(T, K, img_height, img_width, cube: Cuboid):
     U = project(K, cube_cam)
 
     pts = U.T.astype(np.int32)
-    hull = cv.convexHull(pts)
+    hull = cv.convexHull(pts) #[n,1,2]
 
     mask = np.zeros((img_height,img_width), np.uint8)
     mask = cv.fillConvexPoly(mask, hull, 255)
 
     return mask
+
+def roi_hull(T, K, img_height, img_width, cube: Cuboid):
+    cube_vertices = cube.to_vertices()
+    cube_world = cube_vertices
+    cube_cam = W2C_from_pose(T)@cube_world 
+    if (cube_cam[2,:] < 0).any():
+        return np.zeros((img_height,img_width), np.uint8)
+    
+    U = project(K, cube_cam)
+
+    pts = U.T.astype(np.int32)
+    hull = cv.convexHull(pts)
+
+    return hull #[n,1,2]
