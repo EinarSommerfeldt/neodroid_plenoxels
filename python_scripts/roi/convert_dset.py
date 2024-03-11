e3e97cb228939aa4ab5b8f9d1322d50a1d2c8538
Einar Sommerfeldt <einar.sommerfeldt@gmail.com>
2024-03-11 13:06:41 +0100

    fixed list and independace issues

--- python_scripts/roi/convert_dset.py
+++ python_scripts/roi/convert_dset.py
@@ -51,39 +51,44 @@ def expanded_roi(dataset_folder):
 
     K = np.loadtxt(K_path)
 
-    #Create pose array
-    pose_folder_obj = os.scandir(pose_folder)
-    poses = []
-    for p in pose_folder_obj:
-        poses.append(np.loadtxt(pose_folder + os.sep + p.name))
-    poses = np.stack(poses)
+
+    #Create filename array and pose dict
+    folder_obj = os.scandir(pose_folder)
+    filenames = []
+    poses = {}
+    for f in folder_obj:
+        name = f.name.split(".")[0]
+        poses[name] = np.loadtxt(pose_folder + os.sep + name + ".txt")
+        filenames.append(name)
 
     #Create cuboid
     s = 0.1
     cuboid = Cuboid(0, 0, 0, s*3, s*3, s*3) # Colmap coordinate system
     [cuboid.x, cuboid.y, cuboid.z] = [1.8666474370158144, 0.29663802654801896, 2.790420592907028]
 
-    #Find image names
-    folder_obj = os.scandir(image_folder)
-    img_names = [f.name for f in folder_obj]
 
-    for image_index in range(len(img_names)):
-        output_image = cv.imread(image_folder + os.sep + img_names[image_index]) #image to draw lines in
-        Ti = poses[image_index]
-        W2C = W2C_from_pose(Ti) 
-        mask = np.zeros((output_image.shape[0],output_image.shape[1]), np.uint8)
-
-        #Calculate hull of ROI projected into image
-        hull = roi_hull(Ti, K, output_image.shape[0], output_image.shape[1], cuboid) #[n,1,2]
-
-        for pose_index in range(len(poses)):
-            if pose_index==image_index:
-                continue
-            pose_proj = project(K,W2C@poses[pose_index][:,3]) #Project pose into image
-            hull_extended = cv.convexHull(np.vstack([hull, [pose_proj.T.astype(np.int64)]])) #[n,1,2]
-            mask = cv.fillConvexPoly(mask, hull_extended, 255) #Add expanded ROI to mask
+    for filename_img in filenames:
+        output_image = cv.imread(image_folder + os.sep + filename_img + ".png") #image to draw lines in
         
-        output_image = cv.bitwise_and(output_image, output_image, mask=mask)
+        #Project expanded ROI on training images
+        if filename_img.split("_")[1] == "train": #format: 0_train_0000
+            Ti = poses[filename_img]
+            W2C = W2C_from_pose(Ti) 
+            mask = np.zeros((output_image.shape[0],output_image.shape[1]), np.uint8)
+
+            #Calculate hull of ROI projected into image
+            hull = roi_hull(Ti, K, output_image.shape[0], output_image.shape[1], cuboid) #[n,1,2]
+
+            for filename_pose in filenames:
+                if filename_pose == filename_img:
+                    continue
+                if filename_pose.split("_")[1] != "train": #format: 0_train_0000
+                    continue
+                pose_proj = project(K,W2C@poses[filename_pose][:,3]) #Project pose into image
+                hull_extended = cv.convexHull(np.vstack([hull, [pose_proj.T.astype(np.int64)]])) #[n,1,2]
+                mask = cv.fillConvexPoly(mask, hull_extended, 255) #Add expanded ROI to mask
+            
+            output_image = cv.bitwise_and(output_image, output_image, mask=mask)
         
         #Add alpha layer
         rgba = cv.cvtColor(output_image, cv.COLOR_BGR2BGRA)
@@ -91,10 +96,10 @@ def expanded_roi(dataset_folder):
         rgba[:, :, 3] = alpha*255
         output_image = rgba
 
-        cv.imwrite(output_folder + os.sep + img_names[image_index], output_image)
+        cv.imwrite(output_folder + os.sep + filename_img + ".png", output_image)
 
     return 1
 
-dataset_folder = r"C:\Users\einar\Desktop\fruit_roi_scale4"
+dataset_folder = r"C:\Users\einar\Downloads\fruit_roi_scale4_linux"
 #convert_dset(dataset_folder, alpha= True)
 expanded_roi(dataset_folder)
