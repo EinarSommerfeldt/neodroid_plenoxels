d08afc067b02bd7b3fbe7f128a03f8974eadaf1f
Einar Sommerfeldt <einar.sommerfeldt@gmail.com>
2024-05-02 14:32:58 +0200

    check v

--- svox2/opt/sample_roi.py
+++ svox2/opt/sample_roi.py
@@ -96,6 +96,5 @@ cuboid.scale =  dset.scene_scale
 
 v, p = sample_roi(grid, cuboid, grid.radius, grid.links.shape)
 
-
-print(v.shape)
-print(p.shape)
+for elem in v:
+    print(elem.astype(np.float64))
